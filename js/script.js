dea91f4ce8a9beb2544d24da3088f1d7e044f43d
Ovelny <this.is.ovelny@gmail.com>
2016-11-27 13:03:46 +0100

    comments & search-results

--- js/script.js
+++ js/script.js
@@ -18,20 +18,23 @@ function CORSRequest(method, url) {
     return xhr
 }
 
-// display suggestions for user input
+// Watch for changes on search-bar and assign the results
 document.getElementById('search-field').addEventListener('input', function (event) {
     const userInput = document.getElementById('search-field').value
     const searchPredictUrl = 'https://crossorigin.me/http://en.wikipedia.org/w/api.php?action=opensearch&search=' + userInput
 
+    // Make the API call unless CORS isn't supported
     const request = CORSRequest('GET', searchPredictUrl)
     if (!request) {
         throw new Error('CORS is not supported')
     }
 
+    // When request is successful, parse the result as JSON...
     request.onload = function () {
         const response = JSON.parse(request.responseText)
         console.log(response)
 
+        //... unhide the <ul> containing all the suggestions
         let el = document.querySelector(".ghost")
         if (document.querySelector(".ghost") != null) {
             if (el.classList)
@@ -40,14 +43,22 @@ document.getElementById('search-field').addEventListener('input', function (even
                 el.ghost = el.ghost.replace(new RegExp('(^|\\b)' + ghost.split(' ').join('|') + '(\\b|$)', 'gi'), ' ')
         }
 
-        for (let i = 0; i < 5; i++) {
-            let article = response[1][i]
-            let ul = document.getElementById('search-result')
-            ul.insertAdjacentHTML('afterbegin', '<li class="form-autocomplete-item"><div class="chip hand"><div class="chip-content">' + article + '</div></div></li>')
+        //... if suggestions are already present, delete them first
+        let ul = document.getElementById('search-result')
+        if (ul.hasChildNodes()) {
+            while (ul.firstChild) {
+                ul.removeChild(ul.firstChild)
+            }
+        }
 
+        //... and insert suggestions from the API
+        for (let i = 0; i < response[1].length; i++) {
+            let article = response[1][i]
+            ul.insertAdjacentHTML('beforeend', '<li class="form-autocomplete-item"><div class="chip hand"><div class="chip-content"><h6>' + article + '</h6></div></div></li>')
         }
     }
 
+    // If an error occurrs with CORS, throw a warning in console
     request.onerror = function () {
         console.log('error with request')
     }
